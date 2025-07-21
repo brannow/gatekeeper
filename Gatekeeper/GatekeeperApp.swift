9516431f8bb9e0a8d2869ea9865685dddd2bf588
Benjamin Rannow <b.rannow@familie-redlich.de>
2025-07-21 08:02:06 +0700

    Initial Commit

---
+++ Gatekeeper/GatekeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GatekeeperApp.swift
+//  Gatekeeper
+//
+//  Created by Benjamin Rannow on 21.07.25.
+//
+
+import SwiftUI
+
+@main
+struct GatekeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GatekeeperTests/GatekeeperTests.swift
@@ -0,0 +1,17 @@
+//
+//  GatekeeperTests.swift
+//  GatekeeperTests
+//
+//  Created by Benjamin Rannow on 21.07.25.
+//
+
+import Testing
+@testable import Gatekeeper
+
+struct GatekeeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
